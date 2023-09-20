1f00b2f052c9dae96c90498a4e80704d7586e928
Guillaume Vincent <gvincent@redhat.com>
2023-09-20 15:23:43 +0000

    Remove topic team association
    topic team permission will be removed in the future.
    We should remove it from clients first.
    
    Change-Id: I080866a7d9c0d91569948d0ec6faba8457a7186a

--- tests/conftest.py
+++ tests/conftest.py
@@ -286,10 +286,10 @@ def team_user_name(dci_context):
 
 
 @pytest.fixture
-def product_id(dci_context, team_id):
-    return api_product.list(
-        dci_context,
-        where="name:dci_product").json()["products"][0]['id']
+def product_id(dci_context):
+    return api_product.list(dci_context, where="name:dci_product").json()["products"][
+        0
+    ]["id"]
 
 
 @pytest.fixture
@@ -375,14 +375,7 @@ def components_ids(dci_context, topic_id):
 
 
 @pytest.fixture
-def job_factory(
-    dci_context,
-    dci_context_remoteci,
-    team_user_id,
-    remoteci_id,
-    topic_id,
-    components_ids,
-):
+def job_factory(dci_context_remoteci, topic_id):
     def create():
         job = api_job.schedule(dci_context_remoteci, topic_id).json()
         job_id = job["job"]["id"]
@@ -426,7 +419,6 @@ def job_factory(
               name="test_cors_headers" time="0.574683904648"/>
     </testsuite>"""
 
-    api_topic.attach_team(dci_context, topic_id, team_user_id)
     return create
 
 
