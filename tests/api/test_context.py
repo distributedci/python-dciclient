78884f17f7d98b8f7d3e36ac538d8483390f04f3
Haikel Guemar <hguemar@fedoraproject.org>
2024-03-29 11:18:25 +0100

    Display error when client and server time are unsync'ed
    dcictl shows erroneous data when system is unsynchronized
    
    Change-Id: I4e95707effaa59451df8c2e0dde1057674943e86

---
+++ tests/api/test_context.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2017-2022 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.o
+
+from datetime import datetime, timedelta, timezone
+from dciclient.v1.api import topic as api_topic
+from mock import patch
+import pytest
+
+
+def fakeServerTime():
+    t = datetime.now(tz=timezone.utc) - timedelta(minutes=10)
+    return t.strftime("%a, %d %b %Y %X %Z")
+
+
+@patch("dciclient.v1.api.context._extract_date_from_headers",
+       return_value=fakeServerTime())
+def test_list_topics_with_unsynced_time(mock_extract_date, dci_context):
+    with pytest.raises(SystemExit) as e:
+        api_topic.list(dci_context)
+        assert mock_extract_date.called
